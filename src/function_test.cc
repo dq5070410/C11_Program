a1402fa2f7aacc497ef814d2958045582e7d0f07
yanghang <dq5070410@126.com>
2017-03-29 10:19:22 +0800

    std::bind

--- src/function_test.cc
+++ src/function_test.cc
@@ -3,29 +3,31 @@
 
 using namespace std;
 
-class A
+void call_when_even(int x,const std::function<void(int)>& f)
 {
-public:
-	A(const std::function<void()>& f):callback_(f){}
-	void notify(void){
-		callback_();
+	if(!(x & 1))
+	{
+		f(x);
 	}
-private:
-	std::function<void()> callback_;
-};
+}
 
-class Foo
+void output(int x)
 {
-public:
-	void operator()(void){
-		cout << __FUNCTION__ << endl;
-	}
-};
+	cout << x << " ";
+}
 
+void output_add_2(int x)
+{
+	cout << x + 2 << " ";
+}
+
+void f(int a,int b,int c,int d,int e)
+{
+	cout << a << " " << b << " " << c << " " << d << " " << e <<endl;
+}
 int main()
 {
-	Foo foo;
-	A aa(foo);
-	aa.notify();
+	auto g = std::bind(f,10,20,std::placeholders::_2,5,std::placeholders::_1);
+	g(3,7);
 	return 0;
 }
