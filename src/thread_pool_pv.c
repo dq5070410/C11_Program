0647958003954a76d9f472e12491e6b0837a86ed
yanghang <dq5070410@126.com>
2017-04-11 22:00:54 +0800

    again

--- src/thread_pool_pv.c
+++ src/thread_pool_pv.c
@@ -11,4 +11,11 @@ thread_pool *tp_init(int thread_num)
 	if(thread_num < 1)
 		thread_num = 1;
 	tp = (thread_pool *)malloc(sizeof(thread_pool));
+	if(NULL == tp)
+	{
+		printf("Error:allocate memory for thread_pool failed\n");
+		return NULL;
+	}
+	tp->thread_num = thread_num;
+	tp->threads = (pthread_t*)malloc(thread_num * sizeof(pthread_t));
 }
