41446109aa6a850a37e259ba7a4fc3032ee377d7
yanghang <dq5070410@126.com>
2017-08-10 18:36:17 +0800

    mutex

--- src/mutex_test.cc
+++ src/mutex_test.cc
@@ -2,25 +2,31 @@
 #include <thread>         // std::thread
 #include <mutex>          // std::mutex
 
-volatile int counter(0); // non-atomic counter
-std::mutex mtx;           // locks access to counter
+volatile int count(0);
 
-void attempt_10k_increases() {
-    for (int i=0; i<10000; ++i) {
-        if (mtx.try_lock()) {   // only increase if currently not locked:
-            ++counter;
+std::mutex mtx;
+
+void attempt_10k_increase()
+{
+    for(int i = 0;i < 10000;i++)
+    {
+        if(mtx.try_lock())
+        {
+            count++;
             mtx.unlock();
         }
     }
 }
 
-int main (int argc, const char* argv[]) {
+int main()
+{
     std::thread threads[10];
-    for (int i=0; i<10; ++i)
-        threads[i] = std::thread(attempt_10k_increases);
-
-    for (auto& th : threads) th.join();
-    std::cout << counter << " successful increases of the counter.\n";
-
+    for(int i = 0;i < 10;i++)
+    {
+        threads[i] = std::thread(attempt_10k_increase);
+    }
+    for(auto& th:threads)
+        th.join();
+    std::cout << count << "successful increase" << std::endl;
     return 0;
 }
