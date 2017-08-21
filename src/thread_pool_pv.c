1ed99cfcc416c2f16d09461aa71c5e66ee2bfdb1
yanghang <dq5070410@126.com>
2017-08-21 19:07:32 +0800

    thread pool

--- src/thread_pool_pv.c
+++ src/thread_pool_pv.c
@@ -18,4 +18,19 @@ thread_pool *tp_init(int thread_num)
 	}
 	tp->thread_num = thread_num;
 	tp->threads = (pthread_t*)malloc(thread_num * sizeof(pthread_t));
+	if(NULL == tp->threads)
+	{
+		printf("ERROR:allocate memory for threads in thread pool failed\n");
+		return NULL;
+	}
+	if(tp_job_queue_init(tp))
+		return NULL;
+	tp->job_queue->queue_sem = (sem_t *)malloc(sizeof(sem_t));
+	sem_init(tp->job_queue->queue_sem,0,0);
+
+	for( i = 0;i<thread_num;i++)
+	{
+		pthread_create(&(tp->threads[i]),NULL,(void*)tp_thread_func,(void *)tp);
+	}
+	return tp;
 }
