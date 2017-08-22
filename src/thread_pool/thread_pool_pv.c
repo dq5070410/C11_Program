34c80bb2951bcd7975bedbaa6c242538b5a149ba
yanghang <dq5070410@126.com>
2017-08-22 14:44:38 +0800

    thread_pool function

--- src/thread_pool/thread_pool_pv.c
+++ src/thread_pool/thread_pool_pv.c
@@ -34,3 +34,50 @@ thread_pool *tp_init(int thread_num)
 	}
 	return tp;
 }
+
+int tp_job_queue_init(thread_pool *tp)
+{
+	tp->job_queue = (thread_pool_job_queue*)malloc(sizeof(thread_pool_job_queue));
+	if(NULL == tp->job_queue)
+		return -1;
+	tp->job_queue->head = NULL;
+	tp->job_queue->tail = NULL;
+	tp->job_queue->num = 0;
+}
+
+void *tp_thread_func(thread_pool *tp)
+{
+	FUNC function;
+	void *arg_buf;
+	thread_pool_job *job_p;
+
+	while(tp_alive)
+	{
+		if(sem_wait(tp->job_queue->queue_sem))
+		{
+			printf("thread waiting for semphone...\n");
+			exit(1);
+		}
+		if(tp_alive)
+		{
+			pthread_mutex_lock(&mutex);
+			job_p = tp_get_lastjob(tp);
+			if(NULL == job_p)
+			{
+				pthread_mutex_unlock(&mutex);
+				continue;
+			}
+			function = job_p->function;
+			arg_buf = job_p->arg;
+			if(tp_delete_lastjob(tp))
+				return;
+			pthread_mutex_unlock(&mutex);
+			printf("consumer ... get a job from job queue and run it\n");
+			function(arg_buf);
+			free(job_p);
+		}
+		else
+			return;
+	}
+	return;
+}
